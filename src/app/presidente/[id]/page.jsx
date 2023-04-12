490f405bdd4497052d4ef86da73dca5bab93c103
maupeon <maupeon@gmail.com>
2023-04-11 21:03:55 -0500

    Added black page

---
+++ src/app/presidente/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import "../globals.css";
+export default function Presidente() {
+  return (
+    <div className=" w-screen h-screen overflow-hidden ">
+      <div className=" w-full h-full overflow-hidden flex  items-center ">
+        {/* <div className=" relative w-20 h-20  bg-[length:100%] bg-[url('https://vignette4.wikia.nocookie.net/imaspeedrunner/images/3/3a/Orphan_tumbleweed.png/revision/latest?cb=20131006213006')]"></div> */}
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,32 @@ module.exports = {
           "url('https://vignette4.wikia.nocookie.net/imaspeedrunner/images/3/3a/Orphan_tumbleweed.png/revision/latest?cb=20131006213006') no-repeat",
       },
       keyframes: {
+        tumbleweed: {
+          "0%": {
+            transform: "translate(0%, 45px) rotate(0deg)",
+          },
+          "9%": {
+            transform: "translate(15%, 5px) rotate(130deg)",
+          },
+          "17%": {
+            transform: "translate(30%, 45px) rotate(250deg)",
+          },
+          "26%": {
+            transform: "translate(45%, 15px) rotate(360deg)",
+          },
+          "34%": {
+            transform: "translate(60%, 45px) rotate(490deg)",
+          },
+          "42%": {
+            transform: "translate(80%, 30px) rotate(600deg)",
+          },
+          "50%": {
+            transform: "translate(100%, 45px) rotate(720deg)",
+          },
+          "100%": {
+            transform: "translate(100%, 45px) rotate(720deg)",
+          },
+        },
         tilt: {
           "0%, 50%, 100%": {
             transform: "rotate(0deg)",
@@ -33,6 +59,8 @@ module.exports = {
       },
       animation: {
         tilt: "tilt 10s infinite linear",
+        tumbleweed: "tumbleweed linear 3s infinite",
+        // animation-iteration-count: infinite;
       },
     },
   },

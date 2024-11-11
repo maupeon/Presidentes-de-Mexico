10645cd214ca1c3225e2873081312fbbb1b461e7
Mauricio Peón <maupeon@gmail.com>
2024-11-11 16:47:32 -0600

    Update page.jsx

--- src/app/presidente/[id]/page.jsx
+++ src/app/presidente/[id]/page.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Presidente() {
   return (
     <div className=" w-screen h-screen overflow-hidden  scrollbar-hide">
-      <div className=" w-full h-full overflow-hidden flex  items-center scrollbar-hide ">
-        {/* <div className=" relative w-20 h-20  bg-[length:100%] bg-[url('https://vignette4.wikia.nocookie.net/imaspeedrunner/images/3/3a/Orphan_tumbleweed.png/revision/latest?cb=20131006213006')]"></div> */}
-      </div>
+      <div className=" w-full h-full overflow-hidden flex  items-center scrollbar-hide "></div>
     </div>
   );
 }
